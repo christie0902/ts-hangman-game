2db412a4658207dd3bc2af2fc3781a431628530f
Christie Pham <115462474+christie0902@users.noreply.github.com>
2024-04-08 12:07:59 +0200

    set up logic and create components needed

---
+++ src/components/HangmanWord.tsx
@@ -0,0 +1,14 @@
+export function HangmanWord() {
+    const word = "test";
+    const guessedLetters = ['e']
+    return <div style={{ display: "flex", gap: ".25em", fontSize: "6rem", fontWeight: "bold", textTransform: "uppercase", fontFamily: "monospace"}}>
+        {word.split("").map((letter, index)=> (
+            <span key={index} style={{ borderBottom: ".1em solid black"}}>
+                <span
+                style= {{visibility: guessedLetters.includes(letter) ? "visible" : "hidden"}}
+                >{letter}</span>
+                
+                </span>
+            ))}
+    </div>
+}
